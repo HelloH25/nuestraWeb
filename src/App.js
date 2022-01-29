171531cd265534a12f4986e31b270098cd986c88
Michael Forero <mforeroroldan@gmail.com>
2022-01-28 20:40:31 -0500

    barra de navegación implementada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import react from 'react'
 import { Route, Routes } from 'react-router';
 import './App.css';
+import Layouts from './components/Layouts';
 import Contactme from './pages/Contactme';
 import Home from './pages/Home';
 import Nosotros from './pages/Nosotros';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
+      <Layouts/>
       <Routes>
         <Route index element={<Home/>}/>
         <Route path="/Contacto" element={<Contactme/>}/>
--- src/components/Layouts.js
+++ src/components/Layouts.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router-dom';
 
 const Layouts = () => {
   return <div>
-    
+    <Navbar bg="dark" expand="lg">
+                <Container >
+                    <Navbar.Brand ><Link to='/' className=''>ForSab</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" >
+                        <Nav className="me-auto" text-aling="center"  >
+                            <Nav.Link variant="secondary"><Link to='/'>Inicio</Link></Nav.Link>
+                            <Nav.Link><Link to='/nosotros'>Nosotros</Link></Nav.Link>
+                            <Nav.Link><Link to='/contacto'>Contacto</Link></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
   </div>;
 };
 
