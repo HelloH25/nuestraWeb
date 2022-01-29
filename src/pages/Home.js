d0a395edf841ada42891366035edd86a4146a4d6
Michael Forero <mforeroroldan@gmail.com>
2022-01-28 21:28:03 -0500

    primer oferta establecida

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>
-      <h1>INICIO</h1>
-  </div>;
+    return <div>
+        <h1>INICIO</h1>
+        <section>
+            <h3>Bienvenidos a ForSab</h3>
+            <p>Somos una StartUp emergente en el mundo de la tecnología, en donde buscamos conectar y posicionar tu negocio en la internet, para que las visitas y ventas aumenten de una manera significativa</p>
+        </section>
+        <section>
+        <h5>LANDING PAGE</h5>
+            <ul>
+                <li>4 bloque</li>
+
+                <li>1 banner</li>
+                <li>Diseño 100% personalizado</li>
+                <li>redes sociales</li>
+                <li>SEO</li>
+                <li>Diseño responsive (Mobile,Tablet, PC)</li>
+                <li>Hosting y dominio incluido</li>
+                <li>Galería de fotos hasta 15 imágenes Inclusión de un video.</li>
+                <li>Certificado seguridad SSL.</li>
+            </ul>
+        </section>
+    </div>;
 };
 
 export default Home;
+
+
 
